0f672fa1fceb086b6fb013168601af33bc4ce8d1
Guy Eshet <guye@qwak.com>
2023-05-23 10:47:03 +0300

    Adding schema

--- transformers_conda/local_testing.py
+++ transformers_conda/local_testing.py
@@ -1,7 +1,8 @@
 from qwak_inference import RealTimeClient
 
 # Please update MODEL_ID with your model id on Qwak
-QWAK_MODEL_ID = 'your-model-id'
+# QWAK_MODEL_ID = 'huggingface_example'
+QWAK_MODEL_ID = 'huggingface_with_schema'
 
 if __name__ == '__main__':
   feature_vector = [{
