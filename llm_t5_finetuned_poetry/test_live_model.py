382d2bf4b2d1c07156f26015bcfb5e97676c29d8
Guy Eshet <guye@qwak.com>
2023-06-15 08:20:37 +0300

    Updated testing code

--- llm_t5_finetuned_poetry/test_live_model.py
+++ llm_t5_finetuned_poetry/test_live_model.py
@@ -1,14 +1,13 @@
 from qwak_inference import RealTimeClient
 
 # Please update MODEL_ID with your model id on Qwak
-QWAK_MODEL_ID = 'flan_t5_fine_tuned'
+QWAK_MODEL_ID = 'your-model-id'
 
 if __name__ == '__main__':
 
-    for i in range(100):
-        input_ = [{
-            "prompt": "Reporters at the Wall Street Journal interviewed the Russian soldier at a detention facility in the Kharkiv region on May 19. According to the Wall Street Journal, the soldier spoke while under the supervision of a guard. CNN cannot verify whether the soldier spoke under duress."
-        }]
-        client = RealTimeClient(model_id=QWAK_MODEL_ID)
-        response = client.predict(input_)
-        print(response)
+    input_ = [{
+        "prompt": "The 1,000-year-old citadel that rises out of the Atlantic Ocean"
+    }]
+    client = RealTimeClient(model_id=QWAK_MODEL_ID)
+    response = client.predict(input_)
+    print(response)
