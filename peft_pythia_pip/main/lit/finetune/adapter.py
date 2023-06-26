b2b89ac5fbf09eea63f9a91a95aeedd8b7514351
Guy Eshet <guye@qwak.com>
2023-06-26 10:55:11 +0300

    Moving to financial data

--- peft_pythia_pip/main/lit/finetune/adapter.py
+++ peft_pythia_pip/main/lit/finetune/adapter.py
@@ -35,8 +35,11 @@ batch_size = 64 / devices
 micro_batch_size = 4
 gradient_accumulation_iters = batch_size // micro_batch_size
 assert gradient_accumulation_iters > 0
-epoch_size = 50000  # train dataset size
-num_epochs = 1
+
+# train dataset size
+epoch_size = 1000
+num_epochs = 2
+
 max_iters = num_epochs * (epoch_size // micro_batch_size) // devices
 weight_decay = 0.02
 warmup_iters = 2 * (epoch_size // micro_batch_size) // devices  # 2 epochs
