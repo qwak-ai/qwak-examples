9aa5188937e94c881e84d2713babc4d785f17d93
Guy Eshet <guye@qwak.com>
2023-06-27 08:25:17 +0300

    Added Falcon finetuning

---
+++ falcon_finetuned_poetry/main/helpers.py
@@ -0,0 +1,12 @@
+import os
+
+import torch
+
+
+def get_device():
+    pid = os.getpid()
+    return (
+        torch.device("cuda", pid % torch.cuda.device_count())
+        if torch.cuda.is_available()
+        else torch.device("cpu")
+    )
