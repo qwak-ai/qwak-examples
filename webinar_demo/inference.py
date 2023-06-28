b900faf4efa5f75daadb1210c146feb8fefcd47e
Guy Eshet <guye@qwak.com>
2023-06-28 17:16:00 +0300

    updated model id

--- webinar_demo/inference.py
+++ webinar_demo/inference.py
@@ -1,8 +1,8 @@
 from qwak_inference import RealTimeClient
 
 SENTENCE_EMBEDDINGS_MODEL_ID = 'sentence_embeddings'
-FLAN_T5_FINETUNED_MODEL_ID = 'finetuned_flan_t5'
-# FLAN_T5_FINETUNED_MODEL_ID = 'flan_t5_fine_tuned'  # Old model
+# FLAN_T5_FINETUNED_MODEL_ID = 'finetuned_flan_t5'
+FLAN_T5_FINETUNED_MODEL_ID = 'flan_t5_fine_tuned'  # Old model
 FLAN_T5_MODEL_ID = 'flan_t5_large'
 FALCON_7B_MODEL_ID = 'falcon_7b'
 PYTHIA_MODEL_ID = 'parameter_efficient_fine_tuning'
@@ -37,7 +37,7 @@ def falcon_completion(input_text: str, model_id=FALCON_7B_MODEL_ID):
 
 def peft_completion(input_text: str, model_id=PYTHIA_MODEL_ID):
     feature_vector = [{
-        'prompt': input_text
+        'prompt': "question: " + input_text
     }]
     client = RealTimeClient(model_id=model_id)
     response = client.predict(feature_vector)
