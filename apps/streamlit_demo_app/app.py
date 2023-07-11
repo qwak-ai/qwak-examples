44e71d3fcf1846adf1818c6490ccd11765446d4d
Guy Eshet <guyeshet@gmail.com>
2023-07-11 11:30:51 +0300

    Add REST API to streamlit (#29)

--- apps/streamlit_demo_app/app.py
+++ apps/streamlit_demo_app/app.py
@@ -6,22 +6,27 @@ from PIL import Image
 from streamlit_chat import message
 from streamlit_extras.add_vertical_space import add_vertical_space
 
-from inference import falcon_completion, flan_completion, generate_embeddings, \
-    FLAN_T5_FINETUNED_MODEL_ID, FLAN_T5_MODEL_ID, FALCON_7B_MODEL_ID
+from inference import generate_embeddings, FLAN_T5_FINETUNED_MODEL_ID, FLAN_T5_MODEL_ID,\
+    get_qwak_token, get_api_inference
+
+# API_KEY = 'your-key'
+API_KEY = 'eu-5731d1422fb34029b6e60b702ddfb9a3@5m))6Im#MAaDlL0eV1ahL990))ge^9t-'
+
+
+# Fetch a token upon startup
+if 'qwak_token' not in st.session_state:
+    st.session_state['qwak_token'] = get_qwak_token(API_KEY)
+
 
 MODELS = {
     "FLAN T5": {
         "model_id": FLAN_T5_MODEL_ID,
-        "fn": flan_completion
+        "fn": partial(get_api_inference, qwak_token=st.session_state['qwak_token'])
     },
     "Finetuned T5": {
         "model_id": FLAN_T5_FINETUNED_MODEL_ID,
-        "fn": partial(flan_completion, model_id=FLAN_T5_FINETUNED_MODEL_ID)
+        "fn": partial(get_api_inference, qwak_token=st.session_state['qwak_token'])
     },
-    "Falcon 7b": {
-        "model_id": FALCON_7B_MODEL_ID,
-        "fn": falcon_completion
-    }
 }
 
 
@@ -30,8 +35,9 @@ def show_image(image_path):
     st.image(image, use_column_width="always")
 
 
-def generate_response(prompt: str, inference_fn: Callable):
-    return inference_fn(prompt)
+def generate_response(prompt: str, model_id: str, inference_fn: Callable):
+    print(prompt, model_id)
+    return inference_fn(model_input=prompt, model_id=model_id)
 
 
 def get_text():
@@ -67,8 +73,9 @@ with st.container():
         if user_input:
             with st.spinner('Loading...'):
                 response = generate_response(
-                    user_input,
-                    MODELS[current_model]["fn"]
+                    prompt=user_input,
+                    model_id=MODELS[current_model]["model_id"],
+                    inference_fn=MODELS[current_model]["fn"]
                 )
                 if response:
                     message(response)
@@ -77,7 +84,8 @@ with st.container():
             with st.container():
                 with st.spinner("Generating conversation embeddings..."):
                     embedding_input = f"Question: {user_input} Answer: {response}"
-                    embeddings = generate_embeddings(embedding_input)
+                    embeddings = generate_embeddings(input_text=embedding_input,
+                                                     qwak_token=st.session_state['qwak_token'])
                     if embeddings:
                         with st.expander("View embeddings"):
                             st.write(embedding_input)
--- apps/streamlit_demo_app/inference.py
+++ apps/streamlit_demo_app/inference.py
@@ -1,22 +1,84 @@
-from qwak_inference import RealTimeClient
+import requests
 
 SENTENCE_EMBEDDINGS_MODEL_ID = 'sentence_embeddings'
-FLAN_T5_FINETUNED_MODEL_ID = 'flan_t5_fine_tuned'
-FLAN_T5_MODEL_ID = 'flan_t5_large'
+FLAN_T5_FINETUNED_MODEL_ID = 'fine_tuned_flan_t5'
+FLAN_T5_MODEL_ID = 'flan_t5'
 FALCON_7B_MODEL_ID = 'falcon_7b'
 PYTHIA_MODEL_ID = 'parameter_efficient_fine_tuning'
 
 
-def generate_embeddings(input_text: str, model_id=SENTENCE_EMBEDDINGS_MODEL_ID):
-    feature_vector = [{
-        'text': input_text
-    }]
-    client = RealTimeClient(model_id=model_id)
-    response = client.predict(feature_vector)
-    return response[0]
+def get_qwak_token(api_key: str):
+    """
+    Uses Qwak REST API to get an access token
+    :param api_key: Your Qwak API Key
+    :return: a valid Qwak token
+    """
+    response = requests.post(
+        url='https://grpc.qwak.ai/api/v1/authentication/qwak-api-key',
+        headers={
+            'Content-Type': 'application/json',
+        },
+        json={'qwakApiKey': api_key}
+    )
+    if response.ok:
+        return response.json()['accessToken']
+    else:
+        raise ValueError('Missing token')
+
+
+def get_api_inference(model_input: str, model_id: str, qwak_token: str = None, api_key: str = None):
+    """
+    Uses Qwak REST API to fetch model predictions
+    :param model_input: The input string
+    :param model_id: The model ID to fetch
+    :param qwak_token: A valid Qwak token. If kept None, a new one is generated
+    :param api_key: Optional API key to fetch a token on the fly
+    :return: A valid model prediction
+    """
+    if not qwak_token:
+        qwak_token = get_qwak_token(api_key)
+
+    prompt = "question: " + model_input
+
+    res = requests.post(
+        url=f'https://models.llm-demo.qwak.ai/v1/{model_id}/predict',
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {qwak_token}'
+        },
+        json={
+            'columns': ["prompt"],
+            'index': [0],
+            "data": [[prompt]]
+        }
+    )
+
+    return res.json()[0]["generated_text"][0]
+
+
+def generate_embeddings(input_text: str, model_id=SENTENCE_EMBEDDINGS_MODEL_ID, qwak_token: str = None):
+
+    prompt = "Question: " + input_text
+
+    res = requests.post(
+        url=f'https://models.llm-demo.qwak.ai/v1/{model_id}/predict',
+        headers={
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {qwak_token}'
+        },
+        json={
+            'columns': ["text"],
+            'index': [0],
+            "data": [[prompt]]
+        }
+    )
+
+    return res.json()
 
 
 def flan_completion(input_text: str, model_id=FLAN_T5_MODEL_ID):
+    from qwak_inference import RealTimeClient
+
     feature_vector = [{
         'prompt': "Question: " + input_text
     }]
@@ -26,6 +88,8 @@ def flan_completion(input_text: str, model_id=FLAN_T5_MODEL_ID):
 
 
 def falcon_completion(input_text: str, model_id=FALCON_7B_MODEL_ID):
+    from qwak_inference import RealTimeClient
+
     feature_vector = [{
         'prompt': "Provide a short answer: " + input_text
     }]
@@ -35,6 +99,8 @@ def falcon_completion(input_text: str, model_id=FALCON_7B_MODEL_ID):
 
 
 def peft_completion(input_text: str, model_id=PYTHIA_MODEL_ID):
+    from qwak_inference import RealTimeClient
+
     feature_vector = [{
         'prompt': "question: " + input_text
     }]
