8872ea4c7c70a2f332ec340ee8d931e4b6400ecc
Guy Eshet <guye@qwak.com>
2023-09-19 11:31:18 +0300

    updated the tutorial for the vector store

--- vector_store/manage_data.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from sentence_transformers import SentenceTransformer
-
-
-def create_embeddings(input_text: str) -> List[float]:
-    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-    embedding = model.encode([input_text])
-    return embedding.tolist()[0]
