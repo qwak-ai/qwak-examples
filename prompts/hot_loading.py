61f72ef707ac3023cb62cc56c7be752522b852e9
Guy Eshet <guye@qwak.com>
2024-06-06 13:11:16 +0300

    Initial prompt SDK examples

---
+++ prompts/hot_loading.py
@@ -0,0 +1,29 @@
+from time import sleep
+from qwak.llmops.prompt.manager import PromptManager
+
+
+def main(prompt_name: str):
+    prompt_manager = PromptManager()
+
+    while True:
+        # Getting the default prompt version after several seconds
+        # When the default version changes, the loaded prompt will change as well
+        bank_agent_prompt = prompt_manager.get_prompt(
+          name=prompt_name
+        )
+
+        response = bank_agent_prompt.invoke(
+            variables={
+                "question": "Tell me a joke please"
+            }
+        )
+
+        print(response.choices[0].message.content)
+        sleep(10)
+        print()
+
+
+if __name__ == '__main__':
+
+    prompt_name = "banker-agent"
+    main(prompt_name=prompt_name)
