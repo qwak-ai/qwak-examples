2e6ad6d00121c1037ce8a46538b3cd19b5271847
Guy Eshet <guye@qwak.com>
2024-06-11 09:54:46 +0300

    Added description to prompts.json

--- prompts/sdk/hot_loading.py
+++ prompts/sdk/hot_loading.py
@@ -1,29 +1,42 @@
 from time import sleep
+
+from qwak.llmops.model.descriptor import OpenAIChat
 from qwak.llmops.prompt.manager import PromptManager
 
 
 def main(prompt_name: str):
     prompt_manager = PromptManager()
 
-    bank_agent_prompt = prompt_manager.get_prompt(
+    prompt = prompt_manager.get_prompt(
         name=prompt_name
     )
 
     while True:
         # Getting the default prompt version after several seconds
         # When the default version changes, the loaded prompt will change as well
-        response = bank_agent_prompt.invoke(
+        response = prompt.invoke(
             variables={
-                "question": "Tell me a joke please"
-            }
+                "product_name": "Qwak",
+                "product_type": "AI Infra Platform",
+                "question": "what is Qwak?"
+            },
+            stream=True
+            # model_override=OpenAIChat(
+            #         model_id="gpt-4-0613",
+            #         temperature=0.9,
+            #         max_tokens=200
+            # )
         )
 
-        print(response.choices[0].message.content)
+        for chunk in response:
+            print(chunk.choices[0].delta.content)
+
+        # print(response.choices[0].message.content)
         sleep(10)
         print()
 
 
 if __name__ == '__main__':
 
-    prompt_name = "banker-agent"
+    prompt_name = "product-description-for-e-commerce"
     main(prompt_name=prompt_name)
