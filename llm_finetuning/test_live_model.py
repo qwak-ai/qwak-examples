02e93fddeb0901a977764bb4c3cc1322c52e12ec
grig.duta <grig.duta@bunnyshell.com>
2025-08-20 18:00:25 +0300

    Added removed the prompt from the llm output.

--- llm_finetuning/test_live_model.py
+++ llm_finetuning/test_live_model.py
@@ -1,9 +1,10 @@
-import argparse
 import pandas as pd
 from qwak_inference import RealTimeClient
 
+JFML_MODEL_ID = "finetuned_qwen"
 
-def main(model_id):
+
+if __name__ == '__main__':
 
     # Define the columns
     columns = ["prompt"]
@@ -14,22 +15,8 @@ def main(model_id):
     # Create the DataFrame and convert it to JSON
     _input = pd.DataFrame(data, columns=columns).to_json(orient='records')
  
-    client = RealTimeClient(model_id=model_id)
+    client = RealTimeClient(model_id=JFML_MODEL_ID)
     
     print (_input)
     response = client.predict(_input)
     print(pd.DataFrame(response))
-
-
-"""
-USAGE:
-
->> python main/test_live_model.py <your_model_id>
-
-"""
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Predict using the following Qwak model-id.')
-    parser.add_argument('model_id', type=str, help='The Qwak model ID to call for prediction.')
-
-    args = parser.parse_args()
-    main(args.model_id)
